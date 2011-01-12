c1da4933c8e9f4a46cb7bb01dbd110bfe394d3c0
eatcpcks <yannick.schutz@gmail.com>
2011-01-12 21:32:01 +0100

    sinatra redis

--- public/app.js
+++ public/app.js
@@ -241,4 +241,54 @@ $(function() {
             $(this).nextAll('span').removeClass('cross');
         }
     });
+
+	$('#import').click(function(e) {
+			//Cancel the link behavior
+			e.preventDefault();
+			//Get the A tag
+			var id = $(this).attr('href');
+
+			//Get the screen height and width
+			var maskHeight = $(document).height();
+			var maskWidth = $(window).width();
+
+			//Set height and width to mask to fill up the whole screen
+			$('#mask').css({'width':maskWidth,'height':maskHeight});
+
+			//transition effect		
+			$('#mask').fadeIn(1000);	
+			$('#mask').fadeTo("slow",0.8);	
+
+			//Get the window height and width
+			var winH = $(window).height();
+			var winW = $(window).width();
+
+			//Set the popup window to center
+			$('#dialog').css('top',  winH/2-$(id).height()/2);
+			$('#dialog').css('left', winW/2-$(id).width()/2);
+
+			//transition effect
+			$('#dialog').fadeIn(2000); 
+
+		});
+
+		//if close button is clicked
+		$('.window .close').click(function (e) {
+			//Cancel the link behavior
+			e.preventDefault();
+			$('#mask, .window').hide();
+		});		
+
+		//if mask is clicked
+		$('#mask').click(function () {
+			$(this).hide();
+			$('.window').hide();
+		});
+
+
+
+
+
+
+
 });
